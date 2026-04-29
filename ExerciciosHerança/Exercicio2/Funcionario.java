d4c95195c99813581536c66370b17ac7d82202e2
joao25104708360011-dot <joao.25104708360011@faeterj-rio.edu.br>
2026-04-29 10:47:18 -0300

    Create Funcionario.java

---
+++ ExerciciosHerança/Exercicio2/Funcionario.java
@@ -0,0 +1,11 @@
+package ExerciciosHerança.Exercicio2;
+
+public abstract class Funcionario {
+    protected String nome;
+    protected double salario;
+    
+    public Veiculo(String nome, double salario){
+        this.nome = nome;
+        this.salario = salario;
+    }
+}
