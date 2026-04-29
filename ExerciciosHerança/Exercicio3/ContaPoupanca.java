553d63e324e61706f19c83a5d5f492a3721211a5
joao25104708360011-dot <joao.25104708360011@faeterj-rio.edu.br>
2026-04-29 11:20:09 -0300

    Create ContaPoupanca.java

---
+++ ExerciciosHerança/Exercicio3/ContaPoupanca.java
@@ -0,0 +1,25 @@
+package ExerciciosHerança.Exercicio3;
+
+public class ContaPoupanca extends Conta{
+    private int diaRendimento;
+    
+    public ContaPoupanca (String nome, int id, double saldo, int diaRendimento){
+        super(nome, id, saldo);
+        this.diaRendimento = diaRendimento;
+    }
+    
+    @Override
+    public void sacar(double valor){
+        if (this.saldo < valor) {
+            System.out.println("Saldo insuficiente!");
+        } else {
+            this.saldo -= valor;
+            System.out.println("Saque de " + valor + " realizado com sucesso.");
+        }
+    }
+    
+    public void taxas(double taxa){
+        this.saldo = this.saldo + this.saldo * taxa);
+    }
+    
+}
