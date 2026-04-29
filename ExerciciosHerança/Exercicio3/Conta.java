6d4393631ade5547f8fccaabe059b76d6aeb1c00
joao25104708360011-dot <joao.25104708360011@faeterj-rio.edu.br>
2026-04-29 11:17:48 -0300

    Create Conta.java

---
+++ ExerciciosHerança/Exercicio3/Conta.java
@@ -0,0 +1,20 @@
+package ExerciciosHerança.Exercicio3;
+
+public abstract class Conta {
+    protected String nome;
+    protected int id;
+    protected double saldo;
+    
+    public Conta(String nome, int id, double saldo){
+        this.nome = nome;
+        this.id = id;
+        this.saldo = saldo;
+    }
+    
+    public abstract void sacar(double valor);
+    
+    public void depositar(double valor){
+        this.saldo += valor;
+        System.out.println("Valor deposirado com sucesso.");
+    }
+}
