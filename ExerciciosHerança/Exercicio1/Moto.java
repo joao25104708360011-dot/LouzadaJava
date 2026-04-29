045cdff4b0d5cc79816c78fac873304e3f8a193d
joao25104708360011-dot <joao.25104708360011@faeterj-rio.edu.br>
2026-04-29 10:27:54 -0300

    Create Moto.java

---
+++ ExerciciosHerança/Exercicio1/Moto.java
@@ -0,0 +1,20 @@
+package ExerciciosHerança.Exercicio1;
+
+public class Moto extends Veiculo{
+    private float cilindradas;
+    
+    public Moto (String marca, String modelo, float cilindradas){
+        super(marca, modelo);
+        this.cilindradas = cilindradas;
+    }
+    
+    @Override
+    public void exibirDados(){
+        System.out.println("Informações da moto");
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Cilindradas: " + cilindradas);
+        
+    }
+    
+}
