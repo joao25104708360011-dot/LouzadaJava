d885f76ae0611120aea403ba0b1b5d5dce873d22
joao25104708360011-dot <joao.25104708360011@faeterj-rio.edu.br>
2026-04-29 22:55:47 -0300

    Create Funcionario.java

---
+++ ExerciciosHeranca/Exercicio5/Funcionario.java
@@ -0,0 +1,11 @@
+package ExerciciosHeranca.Exercicio5;
+
+public abstract class Funcionario {
+    protected String nome;
+    protected double salario;
+    
+    public Funcionario(String nome, double salario){
+        this.nome = nome;
+        this.salario = salario;
+    }
+}
