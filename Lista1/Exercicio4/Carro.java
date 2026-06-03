5d11c866a3a4a35a15cb6fc494ee3b088a79b6b1
joao25104708360011-dot <joao.25104708360011@faeterj-rio.edu.br>
2026-06-02 22:42:48 -0300

    Create Carro.java

---
+++ Lista1/Exercicio4/Carro.java
@@ -0,0 +1,20 @@
+package Lista1.Exercicio4
+
+public class Carro extends Veiculo implements Tributavel {
+    private int quilometragem;
+    
+    public Carro(String placa, double custoFixo, int quilometragem) {
+        super(placa, custoFixo);
+        this.quilometragem = quilometragem;
+    }
+    
+    @Override
+    public double calcularIPVA(){
+        return (getCustoFixo()*0.04);
+    }
+    
+    @Override
+    public double calcularCustoTotal(){
+        return getCustoFixo() + (quilometragem * 0.15);
+    }
+}
