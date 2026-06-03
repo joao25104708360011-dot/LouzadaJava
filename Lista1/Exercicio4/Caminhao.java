d10a5837853d9d097b6a8affbf6fc9daca075e06
joao25104708360011-dot <joao.25104708360011@faeterj-rio.edu.br>
2026-06-02 22:43:13 -0300

    Create Caminhao.java

---
+++ Lista1/Exercicio4/Caminhao.java
@@ -0,0 +1,20 @@
+package Lista1.Exercicio4
+
+public class Caminhao extends Veiculo implements Tributavel {
+    private double toneladasCarga;
+    
+    public Caminhao(String placa, double custoFixo, double toneladasCarga) {
+        super(placa, custoFixo);
+        this.toneladasCarga = toneladasCarga;
+    }
+    
+    @Override
+    public double calcularIPVA(){
+        return (getCustoFixo()*0.015);
+    }
+    
+    @Override
+    public double calcularCustoTotal(){
+        return getCustoFixo() + (toneladasCarga * 50.0);
+    }
+}
